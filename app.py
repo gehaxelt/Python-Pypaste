a9bb1bbfa5f217fc87518f7b0c3ff61a2a9f1d20
Sebastian Gehaxelt <github@gehaxelt.in>
2014-10-25 22:43:07 +0200

    Retrieve pastes from server

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, request, jsonify
 from config import PyPasteConfig
-import re, time, hashlib, os.path
+import re, time, hashlib, os
 
 
 config = PyPasteConfig()
@@ -16,7 +16,7 @@ def index():
     return app.send_static_file('index.html')
 
 @app.route('/api/createpaste', methods=['POST'])
-def create_paste():
+def createPaste():
     
     # Check if it's a XHR request
     if not request.is_xhr:
@@ -26,7 +26,7 @@ def create_paste():
 
     # Check if it's hex-only
     if not hex_regex.match(data):
-      return jsonify({'error': 'Invalid hex format'}), 200
+        return jsonify({'error': 'Invalid hex format'}), 200
 
     # Check max-length
     if len(data) >= config.getMaxPasteSize():
@@ -38,7 +38,7 @@ def create_paste():
     pastepath = os.path.join('data',pastehash + '.data')
 
     if os.path.exists(pastepath):
-      return jsonify({'error':'Hash collision. Please try again'})
+        return jsonify({'error':'Hash collision. Please try again'}), 200
 
     pastefile = open(pastepath,"w")
     pastefile.write(str(timestamp) + "\n")
@@ -47,6 +47,34 @@ def create_paste():
 
     return jsonify({'error': None, 'hash': pastehash}), 200
 
+@app.route('/api/retrievepaste', methods=['POST'])
+def retrievePaste():
+    # Check if it's a XHR request
+    if not request.is_xhr:
+        return 'Not allowed', 403
+    
+    pastehash = request.form['hash']
+
+    # Check if it's hex-only
+    if not hex_regex.match(pastehash):
+        return jsonify({'error': 'Invalid hash format'}), 200
+
+    pastepath = os.path.join('data',pastehash + '.data')
+
+    if not os.path.exists(pastepath):
+        return jsonify({'error':'Paste does not exist anymore'}), 200
+
+    timestamp = time.time()
+    pastefile = open(pastepath,"r")
+    pastetime = pastefile.readline().strip()
+    pastedata = pastefile.readline().strip()
+    pastefile.close()
+
+    if int(float(timestamp)) >= (config.getMaxLifeTime() + int(float(pastetime))):
+        os.remove(pastepath)
+        return jsonify({'error':'Paste does not exist anymore'}), 200
+
+    return jsonify({'error':None,'data':pastedata}), 200
 
 if __name__ == '__main__':
     app.run(host = config.getHost(),
